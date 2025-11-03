ad02b807d2af9dabc5a8553041dd9ebfa06b6d3d
Jefferson Nascimento <jefferson.nascimento@al.infnet.edu.br>
2025-11-03 16:26:00 -0300

    Update customer.repository.ts

--- src/domain/repositories/customer.repository.ts
+++ src/domain/repositories/customer.repository.ts
@@ -2,7 +2,6 @@ import { Customer } from '../entities/customer.entity';
 import { BaseRepository } from './base.repository';
 
 // aqui a gente pode colocar metodos especificos pro cliente
-// tipo, buscar por email, saca?
 
 export interface CustomerRepository extends BaseRepository<Customer> {
   buscarPorEmail(email: string): Promise<Customer | null>;
